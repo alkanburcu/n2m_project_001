0ce91b93d19bb6df0952e5485c6cfa6306bedd07
Burcu Selin Alkan <bselinalkan@gmail.com>
2026-08-26 11:56:02 +0300

    correct typo

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializer import LoginSerializer
+from .serializers import LoginSerializer
 
 class LoginView(APIView):
     def post(self, request):
