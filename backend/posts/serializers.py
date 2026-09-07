a0ae1c07385cfa6276150ddc8b26c3eb197080a3
Burcu Selin Alkan <bselinalkan@gmail.com>
2026-09-07 17:34:41 +0300

    Add author details to post responses

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -3,14 +3,43 @@ from .models import Comment, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        source="user.username",
+        read_only=True,
+    )
+
+    display_name = serializers.SerializerMethodField()
+
+    def get_display_name(self, obj):
+        return obj.user.name or obj.user.username
+
     class Meta:
         model = Post
 
-        fields = ("id","user","title","body","created_at","updated_at",)
+        fields = (
+            "id",
+            "user",
+            "username",
+            "display_name",
+            "title",
+            "body",
+            "created_at",
+            "updated_at",
+        )
 
-        read_only_fields = ("id","created_at","updated_at",)
+        read_only_fields = (
+            "id",
+            "username",
+            "display_name",
+            "created_at",
+            "updated_at",
+        )
 
-        extra_kwargs = {"user": {"required": False,},}
+        extra_kwargs = {
+            "user": {
+                "required": False,
+            },
+        }
 
     def validate(self, attrs):
         if self.instance is not None:
